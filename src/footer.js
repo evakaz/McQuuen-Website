b6f22edc13e84c059f589560baa77e241475250e
evakaz <evakazakovskaia@gmail.com>
2023-03-17 21:49:08 +0200

    first commit

---
+++ src/footer.js
@@ -0,0 +1,25 @@
+function Footer() {
+    return (
+      <>
+        <div className="float-lef bg-slate-500t">
+          <h3>Kasulikud lingid:</h3>
+          <a href="https://www.tiktok.com/@tiimmqqueen">
+            <img src="/img/tiktok.png" alt="" width="50" height="50" />
+            @tiim_mcqueen
+          </a>
+          <br />
+          <a href="https://www.instagram.com/tiim_mcqueen/">
+            <img src="/img/insta.png" alt="" width="50" height="50" />
+            @tiim_mcqueen
+          </a>
+        </div>
+        <div className="float-right">
+          <h3>Võtke ühendust</h3> <br />
+          <img src="/img/mail.png" alt="" width="55" height="40" /> tiim.mcqueen@gmail.com
+          <br />
+          <img src="/img/adress.png" alt="" width="50" height="50" /> Uus 19, Tallinn
+        </div>
+      </>
+    );
+  }
+  
---
+++ src/header.js
@@ -0,0 +1,26 @@
+function Header() {
+    return (
+      <>
+        <h1>Tiim McQueen</h1>
+        <nav className="bg-red">
+          <ul className="nav-links">
+            <li className="list">
+              <div className="avaleht">
+                <a href="/avaleht.html">Avaleht</a>
+              </div>
+            </li>
+            <li className="list">
+              <a href="/tiimiliikmed.html">Tiimiliikmed</a>
+            </li>
+            <li className="list">
+              <a href="/missioon.html">Meie missioon</a>
+            </li>
+            <li className="list">
+              <a href="/pildimaterjalid.html">Pildimaterjalid</a>
+            </li>
+          </ul>
+        </nav>
+      </>
+    );
+  }
+  
